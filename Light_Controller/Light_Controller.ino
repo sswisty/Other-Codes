a5d319388cfba4c1c54e4f88b5ce60aee18c94e6
Shawn Swist <stb29@wildcats.unh.edu>
2016-09-07 10:37:10 -0400

    First Commit! Addind backbone for code, initalizing all variable names and getting started

---
+++ Light_Controller/Light_Controller.ino
@@ -0,0 +1,117 @@
+/* Light Controller
+This code controls varioius strings of light by using an 8 chanel realy.
+The lights turn on and off in various patters that can be changed.
+The code has support for 6 buttons that turn the lights:
+1) All on
+2) All off
+3) White Lights
+4) Colored Lights
+5) Alternate White and Colored Lights
+6) Random
+Of course these can be changed in the code.
+
+Shawn Swist 2016
+Use for Mills Hall Suite 561
+*/
+
+
+// --------------------- CONSTANT VARIABLES ------------------------
+// Button Pin Locations (some are lowercase as B0, B1 are binary numbers)
+const int b0 = 0;        // Button 0 Reads to Analog Pin 0 
+const int b1 = 1;        // Button 1 Reads to Analog Pin 1
+const int B2 = 2;        // Button 2 Reads to Analog Pin 2
+const int B3 = 3;        // Button 3 Reads to Analog Pin 3
+const int B4 = 4;        // Button 4 Reads to Analog Pin 4
+const int B5 = 5;        // Button 5 Reads to Analog Pin 5
+
+// Relay Pin Locations
+const int R1 = 5;        // Relay 1 Input on Digital Pin 5
+const int R2 = 6;        // Relay 1 Input on Digital Pin 6
+const int R3 = 7;        // Relay 1 Input on Digital Pin 7
+const int R4 = 8;        // Relay 1 Input on Digital Pin 8
+const int R5 = 9;        // Relay 1 Input on Digital Pin 9
+const int R6 = 10;       // Relay 1 Input on Digital Pin 10
+const int R7 = 11;       // Relay 1 Input on Digital Pin 11
+const int R8 = 12;       // Relay 1 Input on Digital Pin 12
+
+// Other Constants
+const int StatLED = 13;  // Status LED on Digital Pin 13
+  // should I make this an RGB LED and then change the color??
+  
+
+
+// --------------------- DYNAMIC VARIABLES ------------------------
+// Used for timing purposes
+int current_time = 0;    // Reads the current time from millis()
+int previoius_time = 0;  // The previous stored time
+int delta_T = 0;         // The difference between current and previous times
+
+// Used for button presses
+int V0 = 0;              // The current voltage on button 0
+int V1 = 0;              // The current voltage on button 1
+int V2 = 0;              // The current voltage on button 2
+int V3 = 0;              // The current voltage on button 3
+int V4 = 0;              // The current voltage on button 4
+int V5 = 0;              // The current voltage on button 5
+
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Initilaize Realy Pins
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
+  pinMode(R3, OUTPUT);
+  pinMode(R4, OUTPUT);
+  pinMode(R5, OUTPUT);
+  pinMode(R6, OUTPUT);
+  pinMode(R7, OUTPUT);
+  pinMode(R8, OUTPUT);
+  
+  // Initilaize Button Pins (need to do this?)
+  pinMode(b0, INPUT);
+  pinMode(b1, INPUT);
+  pinMode(B2, INPUT);
+  pinMode(B3, INPUT);
+  pinMode(B4, INPUT);
+  pinMode(B5, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+}
+
+
+
+
+
+
+
+
+
+
